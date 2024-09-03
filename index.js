8ad4c07927c8d2a411dfadd4461439c244b17638
diananov11 <ohlemon0678@gmail.com>
2024-09-03 20:45:49 +0700

    30 menit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let arr = [8, 3, 9, 2, 5];
-
+let value = arr[0];
 function searchNumber(number) {
   for (let i = 0; i < arr.length; i++) {
     if (number === arr[i]) {
+      console.log(arr[i]);
       number = arr[i];
     } else if (number !== arr[i]) {
       number = false;
+    } else if (Nan) {
+      number = "not a number";
     }
   }
   return number;
