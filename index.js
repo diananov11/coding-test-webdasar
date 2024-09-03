e88bb8599b35236293a574bf49b31f621e4653ab
diananov11 <ohlemon0678@gmail.com>
2024-09-03 20:35:29 +0700

    20 menit

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function searchNumber(number) {
   for (let i = 0; i < arr.length; i++) {
     if (number === arr[i]) {
       number = arr[i];
-    } else if (number !== arr[i]) {
     }
   }
   return number;
 }
 
 console.log(searchNumber(3));
+console.log(searchNumber(1));
+console.log(searchNumber("h"));
