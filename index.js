d323da27dc88f73e6521917547591b093013bb59
diananov11 <ohlemon0678@gmail.com>
2024-09-03 20:30:03 +0700

    15 menit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let arr = [8, 3, 9, 2, 5];
-let value = arr[0];
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === value) {
+
+function searchNumber(number) {
+  for (let i = 0; i < arr.length; i++) {
+    if (number === arr[i]) {
+      number = arr[i];
+    } else if (number !== arr[i]) {
+    }
   }
+  return number;
 }
+
+console.log(searchNumber(3));
